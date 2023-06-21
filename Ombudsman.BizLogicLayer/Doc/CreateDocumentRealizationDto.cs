5b73b6e8b07341c3da9ccc1910f8f443c3f270f6
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-06-21 10:01:34 +0500

    .

--- Ombudsman.BizLogicLayer/Doc/CreateDocumentRealizationDto.cs
+++ Ombudsman.BizLogicLayer/Doc/CreateDocumentRealizationDto.cs
@@ -1,32 +1,13 @@
-﻿
-using System.Text.Json.Serialization;
-
-using Microsoft.AspNetCore.Http;
+﻿using Ombudsman.Core.Interfaces;
+using Ombudsman.Core.Models;
+using Ombudsman.DataLayer;
 
 namespace Ombudsman.BizLogicLayer;
 
-public class CreateDocumentRealizationDto
+public class CreateDocumentRealizationDto : DocumentRealizationDlDto,
+    IEntityDto<CreateDocumentRealizationDto, DocumentRealization>
 {
-    public string DocumentSpeciality { get; set; }
-    public bool IsIncludedInStateProgram { get; set; }
-    public string? StateProgramName { get; set; }
-    public string? DocumentSummary { get; set; }
-    public DateTime SentDate { get; set; }
-    public int ResponsibleSectorId { get; set; }
-    public string MainMinistry { get; set; }
-    public string MainRealizer { get; set; }
-    public string AdditionalRealizers { get; set; }
-    public string Initiative { get; set; }
-    public string? DocNumber { get; set; }
-    public DateTime? DocDate { get; set; }
-    public int DocumentTypeId { get; set; }
-    public int ImportanceId { get; set; }
-    public int InitiativeTypeId { get; set; }
-    public int StateId { get; set; }
-    public string FullName { get; set; }
-    public string ShortName { get; set; }
-    public int DocumentStateId { get; set; }
-    public int OrganizationId { get; set; }
+
     public CreateInformationLetterDto InformationLetter { get; set; }
     public List<CreateDocumentRealizationTableDto> DocumentRealizationTables { get; set; }
 }
---
+++ Ombudsman.DataLayer/Repositories/Doc/DocumentRealizationDlDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ombudsman.DataLayer;
+
+public class DocumentRealizationDlDto
+{
+    public string DocumentSpeciality { get; set; }
+    public bool IsIncludedInStateProgram { get; set; }
+    public string? StateProgramName { get; set; }
+    public string? DocumentSummary { get; set; }
+    public DateTime SentDate { get; set; }
+    public int ResponsibleSectorId { get; set; }
+    public string MainMinistry { get; set; }
+    public string MainRealizer { get; set; }
+    public string AdditionalRealizers { get; set; }
+    public string Initiative { get; set; }
+    public string? DocNumber { get; set; }
+    public DateTime? DocDate { get; set; }
+    public int DocumentTypeId { get; set; }
+    public int ImportanceId { get; set; }
+    public int InitiativeTypeId { get; set; }
+    public int StateId { get; set; }
+    public string FullName { get; set; }
+    public string ShortName { get; set; }
+    public int DocumentStateId { get; set; }
+    public int OrganizationId { get; set; }
+}
